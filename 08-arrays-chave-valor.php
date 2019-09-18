79c407094562d5f92357b41cb9f29f7247734617
buenoyago <55463577+buenoyago@users.noreply.github.com>
2019-09-18 19:21:45 -0300

    Add files via upload

---
+++ 08-arrays-chave-valor.php
@@ -0,0 +1,21 @@
+<?php
+// array chaves - 0 1 2 3 4 5
+$array = array(
+    'luiz' => 40,
+    200,
+    100,
+    3000 => 150,
+    "School of Net",
+    true
+);
+
+echo $array['Yago'];
+echo "\n";
+echo $array[1];
+echo "\n";
+echo $array[3000];
+echo "\n";
+echo $array[2];
+echo "\n";
+var_dump($array);
+echo $array[3001];
