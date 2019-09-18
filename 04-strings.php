af8646a55482f6dcd44319c7a03f632771539cb1
buenoyago <55463577+buenoyago@users.noreply.github.com>
2019-09-18 16:46:10 -0300

    Add files via upload

---
+++ 04-strings.php
@@ -0,0 +1,33 @@
+<?php
+echo "deuzulivre gurizada";
+
+$variavel = "School of Net 
+IAIFEAIFAUHIAUHIAU
+DEUZULIVRE ajofajfauifahfufiauhfahfaiuhafuhfuhfaeiu
+ahfahfiuhfahieafhiuaehiufhaiuhefuihafhuiaf
+";
+
+echo $variavel;
+
+$string1 = "School of Net";
+$string2 = "Iniciando com PHP";
+
+echo $string1 . $string2;
+echo "\n";
+echo "Yago" . " Bueno";
+echo "\n";
+echo $string1 .' '. $string2; 
+
+$string3 = $string1 .' '. $string2; 
+$string3 = "School of Net Iniciando com PHP";
+ 
+$string4 = "$string1 $string2";
+
+echo "\n";
+echo $string4;
+
+$string5 = '$string1 $string2';
+
+echo "\n";
+echo $string5; 
+
