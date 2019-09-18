341a32822b2acec5cb5843097781c082a0fb64f5
buenoyago <55463577+buenoyago@users.noreply.github.com>
2019-09-18 18:33:31 -0300

    Add files via upload

---
+++ 06-comentarios.php
@@ -0,0 +1,28 @@
+<?php
+//Está sendo armazenado a soma de 5 com 3 na variável $variavel
+//texto
+//texto
+$variavel = 5 + 3; //exemplo
+
+/* School of Net */
+/*
+School of Net
+éusguri 
+éusguri
+éusguri
+éusguri
+éusguri
+*/
+
+// comentário de uma linha
+/* comentário multilinha */
+
+/***************************************
+ * 
+ * Texto 
+ * 
+ * 
+ * 
+ ****************************************/
+
+ /******* deuzulivre *************/
