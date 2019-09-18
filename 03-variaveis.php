9cc4776fe652c5f07c9f7c4c2877810e0180de62
buenoyago <55463577+buenoyago@users.noreply.github.com>
2019-09-18 15:36:43 -0300

    Add files via upload

---
+++ 03-variaveis.php
@@ -0,0 +1,38 @@
+<?PHP
+$nome = "School of Net";
+
+echo $nome;
+
+$nome= "Iniciando com PHP";
+
+echo "\n";
+
+echo $nome;
+
+$nome = 5000;
+
+echo "\n";
+
+echo $nome;
+
+$numero = 5000;
+
+echo "\n";
+
+echo $numero;
+
+$NOME = "school of net";
+$NOME234 = "school of net outra variavel";
+$nome2213nome = "deuzulivre";
+$primeiro_nome = "yaguera";
+$primeiroNome = "yaguera";
+
+$variavelTrue = true;
+$variavelFalse = false;
+
+echo "\n";
+echo $variavelTrue;
+echo "\n";
+echo $variavelFalse;
+
+
