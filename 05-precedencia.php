70d17695cbdb483737e11a81a5dd9e201ca5944f
buenoyago <55463577+buenoyago@users.noreply.github.com>
2019-09-18 17:39:02 -0300

    Add files via upload

---
+++ 05-precedencia.php
@@ -0,0 +1,22 @@
+<?php
+
+$soma = 5 + 3 * 8;
+$soma = 5 + 24;
+$soma = 29; 
+
+//esquerda --------> direita
+
+$string = 'School of Net'.'Iniciando com PHP';
+$string = 'School of NetIniciando com PHP';
+
+echo 5 + 3 * 8;
+
+echo "\n";
+
+$soma = (5 + 3 * 5) * 8;
+
+echo $soma; 
+
+//Adicionei o echo "\n"(não tinha no código da aula) de forma com que separasse o resultado das operações
+
+
