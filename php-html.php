adb4a6c0d6f6b2c3633022c1e557ab8bb0c617ad
buenoyago <55463577+buenoyago@users.noreply.github.com>
2019-09-18 20:17:49 -0300

    Add files via upload

---
+++ php-html.php
@@ -0,0 +1,15 @@
+<?php
+    $nome = "Yago Bueno";
+    $html = "<h1>Iniciando com PHP</h1>"
+?>
+<html>
+    <head></head>
+    <body>
+        <?php echo 5 + 3; 
+              echo "\n";
+              echo $nome;
+        ?>
+        <?php echo $nome; ?>
+        <?php echo $html; ?>
+    </body>
+</html>
