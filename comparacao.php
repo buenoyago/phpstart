c797010828784f07532032d8315f15b111089d03
buenoyago <55463577+buenoyago@users.noreply.github.com>
2019-09-19 07:16:39 -0300

    Add files via upload

---
+++ comparacao.php
@@ -0,0 +1,16 @@
+<?php
+
+$a = 10;
+$b = 10;
+
+/*if ($a != $b) {
+    echo "A é diferente a B";
+} else {
+    echo "A é igual de B";
+}*/
+
+if ($a >= $b) {
+    echo "A é maior ou igual do que B!";
+} else {
+    echo "A é menor do que B";
+}
