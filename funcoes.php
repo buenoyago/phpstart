e11be1041a6ba000fc49b21ae0f42e0dbbddaece
buenoyago <55463577+buenoyago@users.noreply.github.com>
2019-09-24 11:26:50 -0300

    Add files via upload

---
+++ funcoes.php
@@ -0,0 +1,15 @@
+<?php
+
+function escreva_nome($nome) {
+    echo "Bom dia querido(a) aluno(a) $nome!<br>";
+}
+
+escreva_nome("Yago");
+
+escreva_nome("Brenda");
+
+escreva_nome("Matilde");
+
+echo "Olá querido aluno Yago!<br>";
+echo "Olá querido aluno Brenda!<br>";
+echo "Olá querido aluno Matilde!<br>";
